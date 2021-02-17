69d69837fb0893731b06f1ff70bd347579d04ee9
Angel Miguel Loor Manzano <miguelangelloor96@gmail.com>
2021-02-17 13:32:33 -0500

    Subiendo a github

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        "publicPath": '/',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        hot: true,
+        open: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                },
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    },
+                ],
+            },
+            {
+                test: /\.(s*)css$/,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|gif|jpg)$/,
+                use: [
+                    {
+                        'loader': 'file-loader',
+                        options: {
+                            name: 'assets/[hash].[ext]',
+                        },
+                    },
+                ]
+            },
+
+        ]
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebPackPlugin({
+            template: './public/index.html',
+            filename: './index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].css',
+        })
+    ]
+};
